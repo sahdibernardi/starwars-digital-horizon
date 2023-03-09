d8012613ddf6e0b342bbeae1609a8a8456137e9a
Sahra Di Bernardi <sah.dibernardi@gmail.com>
2023-03-09 15:08:24 -0300

    fix: deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, HashRouter } from 'react-router-dom';
 import EachStarship from './components/EachStarship';
 import Home from './components/Home';
 import People from './components/People';
@@ -12,11 +12,13 @@ function App() {
   return (
     <AppProvider>
       <BrowserRouter>
+      <HashRouter basename="/">
         <Route exact path="/" component={ Home }/>
         <Route path="/planets" component={ Planets }/>
         <Route path="/starships" component={ Starships }/>
         <Route path="/each-starship" component={ EachStarship }/>
         <Route path="/people" component={ People }/>
+      </HashRouter>
       </BrowserRouter>
     </AppProvider>
   );
